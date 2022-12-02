8dd0ef4d7a8f5c4c77712fc7804c9f497ab41c84
Rob Moorman <rob@moori.nl>
2022-12-02 10:54:12 +0100

    chore: version bump 0.3.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ docs_require = []
 
 setup(
     name="wagtail-analytics",
-    version="0.3.1",
+    version="0.3.2",
     description="",
     author="Moori",
     install_requires=install_requires,
